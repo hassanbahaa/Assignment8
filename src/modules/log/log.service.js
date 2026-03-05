370afd9c1393ef9e9271275735126feb3c602b86
Hassan Bahaa <66232742+hassanbahaa@users.noreply.github.com>
2026-03-06 00:19:01 +0200

    refactor: separate collection setup into its own module

--- src/modules/log/log.service.js
+++ src/modules/log/log.service.js
@@ -1,12 +1,2 @@
 import { db } from "../../db/connect.js";
 import { Log } from "../../db/models/log.model.js";
-
-export const CreateLogsCollection = async () => {
-  // capped collection
-  const log = await db.createCollection("logs", {
-    capped: true,
-    size: 1024 * 1024,
-  });
-  if (!log) throw new Error("Error creating the collection", { cause: 500 });
-  return log;
-};
