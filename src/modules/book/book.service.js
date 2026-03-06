2cb7a40fa9ab64f61b3583cb5eac8613681705fc
Hassan Bahaa <66232742+hassanbahaa@users.noreply.github.com>
2026-03-06 03:22:57 +0200

    feat: add read operations for books collection

--- src/modules/book/book.service.js
+++ src/modules/book/book.service.js
@@ -14,3 +14,62 @@ export const InsertManyBooks = async (books) => {
   if (!insert) throw new Error("Error inserting the documents", { cause: 500 });
   return insert;
 };
+
+// Q-8 update book with title
+export const UpdateBookByTitle = async (title, update) => {
+  const updateResult = await Book.updateOne({ title }, { $set: update });
+  if (!updateResult)
+    throw new Error("Error updating the document", { cause: 500 });
+  return updateResult;
+};
+
+// Q9 find book with title
+export const getBookByTitle = async (title) => {
+  const book = await Book.findOne({ title });
+  if (!book) throw new Error("Book not found", { cause: 404 });
+  return book;
+};
+
+//  Q10 get books by year range
+export const getBooksByYear = async (from, to) => {
+  const books = await Book.find({ year: { $gte: from, $lte: to } }).toArray();
+  if (!books || books.length === 0)
+    throw new Error("No books found in the given year range", { cause: 404 });
+  return books;
+};
+
+// Q11 get books by genre
+export const getBooksByGenre = async (genres) => {
+  const books = await Book.find({ genres }).toArray();
+  if (!books || books.length === 0)
+    throw new Error("No books found in the given genre", { cause: 404 });
+  return books;
+};
+
+// Q12 Skip the first two books, limit the results to the next three, sorted by year in descending order
+export const getBooksWithPagination = async () => {
+  const books = await Book.find().skip(2).limit(3).sort({ year: -1 }).toArray();
+  if (!books || books.length === 0)
+    throw new Error("No books found with the given pagination", { cause: 404 });
+  return books;
+};
+
+//Q13 Find books where the year field stored as an integer
+export const findBookswithInt = async () => {
+  const books = await Book.find({ year: { $type: "int" } }).toArray();
+  if (!books || books.length === 0)
+    throw new Error("No books found with year as integer", { cause: 404 });
+  return books;
+};
+
+// Q14 Find all books where the genres field does not include any of the genres "Horror" or "Science Fiction"
+export const findBooksNotInGenres = async () => {
+  const books = await Book.find({
+    genres: { $not: { $in: ["Horror", "Science Fiction"] } },
+  }).toArray();
+  if (!books || books.length === 0)
+    throw new Error("No books found with the given genre criteria", {
+      cause: 404,
+    });
+  return books;
+};
